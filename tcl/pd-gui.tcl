6304e99afb70a6b1c9307bfdca67b8157df5e648
Seb Shader <sebfumaster@aol.com>
2020-04-11 13:52:46 -0700

    let txt_hilight and canvas_fill be empty
    so it can be set by the system

--- tcl/pd-gui.tcl
+++ tcl/pd-gui.tcl
@@ -237,7 +237,7 @@ set redo_action no
 array set ::pd_colors {
 selection_rectangle "#000"
 selected "#00f"
-txt_highlight "#C3C3C3"
+txt_highlight ""
 msg_iolet "#000"
 signal_iolet "#000"
 msg_cord "#000"
@@ -397,7 +397,6 @@ proc init_for_platform {} {
             set ::cursor_editmode_connect "circle"
             set ::cursor_editmode_disconnect "X_cursor"
             set ::cursor_editmode_resize "sb_h_double_arrow"
-            set ::pd_colors(txt_highlight) "SystemHighlight"
         }
         "win32" {
             set ::modifier "Control"
@@ -448,7 +447,6 @@ proc init_for_platform {} {
             set ::cursor_editmode_connect "circle"
             set ::cursor_editmode_disconnect "X_cursor"
             set ::cursor_editmode_resize "sb_h_double_arrow"
-            set ::pd_colors(txt_highlight) "SystemHighlight"
         }
     }
 }
