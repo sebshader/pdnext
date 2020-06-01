6ff5dd9b24c41c88193528441f35649f9a94627f
Seb Shader <sebfumaster@aol.com>
2020-06-01 02:23:59 -0700

    up splinesteps to 36
    looks a bit better

--- tcl/pdtk_canvas.tcl
+++ tcl/pdtk_canvas.tcl
@@ -475,12 +475,12 @@ proc ::pdtk_canvas::pdtk_connect {x1 y1 x2 y2 width tags canv} {
 		}
 		${canv} create line $x1 $y1 $x1 [expr {$y1 + $yoff}] \
 			[expr {$x1 + $halfx}] [expr {$y1 + $halfy}] $x2 \
-			[expr {$y2 - $yoff}] $x2 $y2 -smooth 1 -splinesteps 16 \
+			[expr {$y2 - $yoff}] $x2 $y2 -smooth 1 -splinesteps 36 \
 			-width $width -tags $tags
 	} else {
 		# have to set smooth and splinesteps in case it changes
 		${canv} create line $x1 $y1 $x2 $y2 -width $width -tags $tags \
-			-smooth 1 -splinesteps 16
+			-smooth 1 -splinesteps 36
 	}
 }
 
